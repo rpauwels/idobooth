5fb32c1017cde7ac6b954c6f0fb86a7ae95cfead
Raf Pauwels <saftaplan@gmail.com>
2018-01-22 08:22:11 +0100

    Slideshow via feh only partly working

--- photobooth.py
+++ photobooth.py
@@ -2,6 +2,9 @@
 
 import os
 import logging
+import subprocess
+import time
+import signal
 import RPi.GPIO as GPIO
 import picamera
 
@@ -10,33 +13,41 @@ R_BUTTON_PIN = 27
 IMG_FOLDER = "foto"
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(L_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # start taken pics
-GPIO.setup(R_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # exit program
+GPIO.setup(L_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(R_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def takePicture(camera, countdown):
+def takePicture(camera, countdown, filename):
     while countdown > 0:
         countdown -= 1
         camera.annotate_text = str(countdown)
         time.sleep(1)
-    camera.annotate_text = 'smile'
-    timestamp = time.time()
-    timestampAsString = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d_%H-%M-%S')
-    camera.capture(IMG_FOLDER + os.pathsep + str(timestampAsString) + '.jpg')
+    camera.annotate_text = ''
+    for i in xrange(50,100,10):
+        camera.brightness = i
+    logging.info("Capturing")
+    camera.capture(os.path.join(IMG_FOLDER, filename))
+    logging.info("Restoring framerate")
+    camera.brightness = 50
 
-log = logging.getLogger()
 if not os.path.exists(IMG_FOLDER):
     os.mkdir(IMG_FOLDER)
+logging.basicConfig(level=logging.DEBUG)
 with picamera.PiCamera() as camera:
     camera.annotate_text_size = 160
-    camera.vflip = True;
-    logging.info("Initialized and waiting for button press")
+    #camera.vflip = True
+    logging.info("Running fbi")
+    slideshow = subprocess.Popen(['fbi'] +
+        [os.path.join(IMG_FOLDER, f) for f in os.listdir(IMG_FOLDER)])
     while True:
-        
         if (GPIO.input(L_BUTTON_PIN) == False):
             logging.info("Left button pressed")
         elif (GPIO.input(R_BUTTON_PIN) == False):
             logging.info("Right button pressed")
+            #slideshow.terminate()
             camera.start_preview()
-            takePicture(camera, 8, "1.jpg")
-            takePicture(camera, 5, "2.jpg")
-            takePicture(camera, 5, "3.jpg")
+            prefix = time.strftime('%y%m%d%H%M%S')
+            takePicture(camera, 8, prefix + "-1.jpg")
+            takePicture(camera, 5, prefix + "-2.jpg")
+            takePicture(camera, 5, prefix + "-3.jpg")
+            #slideshow = subprocess.Popen(['fbi', os.path.join(IMG_FOLDER, "*.jpg")])
+            camera.stop_preview()
