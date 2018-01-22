865cd8afef9a983b5e58f4aed3e07e9f4cd95778
saftaplan <saftaplan@example.com>
2018-01-22 18:37:07 +0100

    Added brightness delays Added fbi killing Added pygame experiment

--- photobooth.py
+++ photobooth.py
@@ -4,9 +4,11 @@ import os
 import logging
 import subprocess
 import time
-import signal
+import psutil
 import RPi.GPIO as GPIO
 import picamera
+
+import pygame
 
 L_BUTTON_PIN = 23
 R_BUTTON_PIN = 27
@@ -17,37 +19,67 @@ GPIO.setup(L_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(R_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def takePicture(camera, countdown, filename):
+    camera.start_preview()
     while countdown > 0:
         countdown -= 1
         camera.annotate_text = str(countdown)
         time.sleep(1)
     camera.annotate_text = ''
-    for i in xrange(50,100,10):
+    for i in xrange(50,100,5):
         camera.brightness = i
+        time.sleep(0.05)
     logging.info("Capturing")
-    camera.capture(os.path.join(IMG_FOLDER, filename))
-    logging.info("Restoring framerate")
+    path = os.path.join(IMG_FOLDER, filename)
+    camera.capture(path)
+    camera.stop_preview()
+    displayImage(path)
     camera.brightness = 50
 
+def kill(pname):
+    for proc in psutil.process_iter():
+        if proc.name() == pname:
+            proc.terminate()
+
+def displayImage(screen, image):
+    image = pygame.image.load(image)
+    screen.blit(image, image.get_rect())
+    pygame.display.flip()
+
+def slideshow(screen):
+    while (slideshowRunning):
+        for f in os.listdir(IMG_FOLDER):
+            displayImage(screen, f)
+            timer.sleep(1)
+
+logging.basicConfig(level=logging.DEBUG)
 if not os.path.exists(IMG_FOLDER):
     os.mkdir(IMG_FOLDER)
-logging.basicConfig(level=logging.DEBUG)
+
+pygame.init()
+disp_info = pygame.display.Info
+screen = pygame.display.set_mode((disp_info.current_w, disp_info.current_h))
+slideshowRunning = True
+t = Thread(target=slideshow)
+t.start()
+
 with picamera.PiCamera() as camera:
     camera.annotate_text_size = 160
-    #camera.vflip = True
+    camera.vflip = True
     logging.info("Running fbi")
-    slideshow = subprocess.Popen(['fbi'] +
-        [os.path.join(IMG_FOLDER, f) for f in os.listdir(IMG_FOLDER)])
+    #slideshow = subprocess.Popen(['fbi'] +
+    #    [os.path.join(IMG_FOLDER, f) for f in os.listdir(IMG_FOLDER)])
     while True:
         if (GPIO.input(L_BUTTON_PIN) == False):
             logging.info("Left button pressed")
         elif (GPIO.input(R_BUTTON_PIN) == False):
             logging.info("Right button pressed")
-            #slideshow.terminate()
-            camera.start_preview()
+            #kill('fbi')
+            #camera.start_preview()
             prefix = time.strftime('%y%m%d%H%M%S')
+            slideshowRunning = False
             takePicture(camera, 8, prefix + "-1.jpg")
             takePicture(camera, 5, prefix + "-2.jpg")
             takePicture(camera, 5, prefix + "-3.jpg")
             #slideshow = subprocess.Popen(['fbi', os.path.join(IMG_FOLDER, "*.jpg")])
-            camera.stop_preview()
+            #camera.stop_preview()
+            slideshowRunning = True
