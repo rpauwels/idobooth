5dbde39c1539dcd6fe1711b501270eb26734db25
saftaplan <saftaplan@example.com>
2018-01-23 18:04:23 +0100

    Global variables, per-dir slideshow

--- photobooth.py
+++ photobooth.py
@@ -32,17 +32,15 @@ def takePicture(camera, countdown, filename):
     path = os.path.join(IMG_FOLDER, filename)
     camera.brightness = 50
     camera.capture(path)
-    displayImage(path)
+    renderImage(path)
     camera.stop_preview()
     time.sleep(5)
 
-def displayImage(file):
-    screen.fill((0,0,0))
+def renderImage(file):
+    #screen.fill((0,0,0))
     if not file in image_cache:
-        logging.info("Loading " + file)
         image_cache[file] = pygame.image.load(file)
     image = image_cache[file]
-    logging.info("Loaded " + file + " from cache")
     screen.blit(image, (0, 0))
 
 def renderText(text, x, y, right):
@@ -50,29 +48,50 @@ def renderText(text, x, y, right):
     rendered_text = font.render(text, True, (255, 255, 255))
     text_rect = rendered_text.get_rect()
     if right:
-        text_rect.right = x
+        text_rect.right = screen.right - x
     else:
         text_rect.left = x
     text_rect.top = y
     screen.blit(rendered_text, text_rect)
+
+def slideshow():
+    global slideshow_running
+    for dir in sorted(os.listdir(IMG_FOLDER)):
+        for i in range(0,3):
+            for file in sorted(os.listdir(os.path.join(IMG_FOLDER, dir))):
+                renderImage(os.path.join(IMG_FOLDER, dir, file))
+                renderText("1. kies je props", 700, 0, False)
+                renderText("2. druk op de zwarte knop voor foto's", 700, 30, False)
+                #renderText("   druk op de rode knop voor een filmpje", 700, 30, False)
+                renderText("3. smile!", 700, 60, False)
+                pygame.display.flip()
+                time.sleep(0.5)
+                if not slideshow_running:
+                    logging.info("Slideshow stopped")
+                    screen.fill((0,0,0))
+                    pygame.display.flip()
+                    return
+            time.sleep(0.5)
                     
 def leftButton(channel):
     logging.info("Left button pressed")
 
 def rightButton(channel):
-    logging.info("Right button pressed " + slideshow_running)
+    global slideshow_running
     slideshow_running = False
+    logging.info("Right button pressed")
+    dir = time.strftime('%y%m%d-%H%M%S')
+    os.mkdir(dir)
     with picamera.PiCamera() as camera:
         camera.annotate_text_size = 160
         camera.vflip = True
-        prefix = time.strftime('%y%m%d%H%M%S')
-        takePicture(camera, 8, prefix + "-1.jpg")
-        takePicture(camera, 5, prefix + "-2.jpg")
-        takePicture(camera, 5, prefix + "-3.jpg")
+        takePicture(camera, 8, dir, os.path.join(dir, "1.jpg"))
+        takePicture(camera, 5, dir, os.path.join(dir, "2.jpg"))
+        takePicture(camera, 5, dir, os.path.join(dir, "3.jpg"))
     slideshow_running = True
 
-GPIO.add_event_detect(R_BUTTON_PIN, GPIO.FALLING, callback=rightButton, bouncetime=1000)
-GPIO.add_event_detect(L_BUTTON_PIN, GPIO.FALLING, callback=leftButton, bouncetime=1000)
+GPIO.add_event_detect(R_BUTTON_PIN, GPIO.FALLING, callback=rightButton, bouncetime=500)
+GPIO.add_event_detect(L_BUTTON_PIN, GPIO.FALLING, callback=leftButton, bouncetime=500)
 
 logging.basicConfig(level=logging.DEBUG)
 if not os.path.exists(IMG_FOLDER):
@@ -86,12 +105,5 @@ screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 while True:
     time.sleep(0.1)
     if slideshow_running:
-        logging.info("Restarting slideshow")
-        for f in sorted(os.listdir(IMG_FOLDER)):
-            displayImage(os.path.join(IMG_FOLDER, f))
-            renderText("druk op de rechtse knop", 0, 0, True)
-            pygame.display.flip()
-            time.sleep(1)
-            if not slideshow_running:
-                logging.info("Slideshow stopped")
-                break
+        slideshow()
+
